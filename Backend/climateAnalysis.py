2863e769826d440053eb84559cb0ca72eb85b43c
Raghav Saboo <44055129+raghs2000@users.noreply.github.com>
2019-10-19 10:09:55 -0400

    Add files via upload

--- Backend/climateAnalysis.py
+++ Backend/climateAnalysis.py
@@ -6,12 +6,11 @@ class climateAnalysis:
     self.coord = []
 
   def getCoord():
-    request = requests.get("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?api_key=gbZEcVePozgwaMfpr29gbmZL0LkCdZE8IVLr7dr4")
+    request = requests.get("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?api_key"
+                           "=gbZEcVePozgwaMfpr29gbmZL0LkCdZE8IVLr7dr4")
     print(request.status_code)
     request_json = request.json()
-    request_json = request_json['events']
-
+    request_json = request_json.get('events')
+    print(request_json)
 
   getCoord()
-
-
